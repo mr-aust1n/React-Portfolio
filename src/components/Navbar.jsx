45332e419c84579960c56edb3c19c001934706fb
MrAust1n <craig@mraustin.co.uk>
2024-01-16 21:03:43 +0000

    open close nav done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react';
+import styles from "./Navbar.module.css";
+import { getImageUrl } from '../utils';
+
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <nav className={StyleSheet.navbar}>
-        <a href='/'>Portfolio</a>
-        <div>
-            <ul>
+    <nav className={styles.navbar}>
+        <a className={styles.title}href='/'>Portfolio</a>
+        <div className={styles.menu}>
+        <img className={styles.menuBtn} src={
+            menuOpen 
+            ? getImageUrl("nav/closeIcon.png")
+            : getImageUrl("nav/menuIcon.png")
+        } alt='menu-butting' 
+        onClick={() => setMenuOpen(!menuOpen)}/>
+            <ul className={styles.menuItems}>
             <li><a href='#about'></a>About</li>
             <li><a href='#about'></a>Experience</li>
             <li><a href='#about'></a>Projects</li>
