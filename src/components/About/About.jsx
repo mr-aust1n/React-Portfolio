c1d1b80f1d62d888ca6a6a2c2a59732446dc93db
MrAust1n <craig@mraustin.co.uk>
2024-01-18 17:01:49 +0000

    about section done

---
+++ src/components/About/About.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+
+export const About = () => {
+  return (
+  <section className={styles.container} id="about">
+    <h2 className={styles.title}>About</h2>
+<div className={styles.content}>
+    <img src={getImageUrl('about/aboutImage.png')} alt="Craig with a laptop on my lap" className={styles.aboutImage} />
+    <ul className={styles.aboutItems}>
+        <li className={styles.aboutItem}>  <img src={getImageUrl('about/cursorIcon.png')} alt="Mouse Icon" />    
+    <div className={styles.aboutItemText}>
+        <h3>Front-End Developer</h3>
+       <p>I am a front end dev dont you</p>
+    </div>
+    </li> 
+    <li className={styles.aboutItem}>  <img src={getImageUrl('about/serverIcon.png')} alt="Mouse Icon" /> 
+    <div className={styles.aboutItemText}>
+        <h3>Front-End Developer</h3>
+       <p>I am a front end dev dont you</p>
+    </div>
+    </li>   
+    <li className={styles.aboutItem}>  <img src={getImageUrl('about/uiIcon.png')} alt="Mouse Icon" />     
+    <div className={styles.aboutItemText}>
+        <h3>Front-End Developer</h3>
+       <p>I am a front end dev dont you</p>
+    </div>
+    </li>   
+    </ul>  
+</div>
+
+  </section>
+  )
+}
