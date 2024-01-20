3da312807bfee52764925f4627a58da1b65671c0
MrAust1n <craig@mraustin.co.uk>
2024-01-20 18:51:57 +0000

    Experience complete

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from "./Projects.module.css";
+//import skills from "../../data/projects.json";
+import { getImageUrl } from "../../utils";
+
+export const Projects = () => {
+  return (
+    <div>Projects</div>
+  )
+}
